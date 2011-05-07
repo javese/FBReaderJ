0629622aba498cda63662fd2a018841ca0bf30ff
Nikolay Pultsin <geometer@fbreader.org>
2011-05-07 22:08:13 +0100

    code simplification

--- src/org/geometerplus/android/fbreader/tips/TipsActivity.java
+++ src/org/geometerplus/android/fbreader/tips/TipsActivity.java
@@ -30,6 +30,7 @@ import org.geometerplus.zlibrary.ui.android.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Html;
 import android.text.SpannableStringBuilder;
 import android.text.method.LinkMovementMethod;
 import android.text.style.URLSpan;
@@ -45,18 +46,18 @@ public class TipsActivity extends Activity {
 	@Override
 	public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
-		
+
 		ZLIntegerOption dateOpt = TipsHelper.getDateOprion();
 		int lastDate = dateOpt.getValue();
-		if (lastDate == new Date().getDate()){ 				
+		if (lastDate == new Date().getDate()) {
 //		if (lastDate == new Date().getDate() && false){ 	// for testing
 			finish();
 			return;
 		}
-		
+
 		setContentView(R.layout.tip_dialog);
-		
-		final ZLResource dialogResource = ZLResource.resource("dialog");	
+
+		final ZLResource dialogResource = ZLResource.resource("dialog");
 		final TextView textView = ((TextView)findViewById(R.id.plugin_dialog_text));
 		final CheckBox checkBox = (CheckBox)findViewById(R.id.plugin_dialog_checkbox);
 		checkBox.setText(dialogResource.getResource("tips").getResource("dontShowAgain").getValue());
@@ -81,60 +82,20 @@ public class TipsActivity extends Activity {
 				new TipsHelper(myTipFeedListener).showTip();
 			}
 		});
-		
+
 		myTipFeedListener = new ITipFeedListener() {
 			@Override
 			public void tipFound(ITip tip) {
 				setTitle(tip.getTipTitle());
-				textView.setText(getAnalyzeText(tip.getTipContext()));
+				textView.setText(Html.fromHtml(tip.getTipContext()));
 				textView.setMovementMethod(LinkMovementMethod.getInstance());
 			}
 		};
 		new TipsHelper(myTipFeedListener).showTip();
 		getWindow().setLayout(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
-	}	
-	
+	}
+
 	private void dontShowAction(){
 		TipsHelper.getShowOption().setValue(false);
 	}
-		
-	private CharSequence getAnalyzeText(String originalText) {
-		final SpannableStringBuilder builder = new SpannableStringBuilder();
-		while (true) {
-			int index = originalText.indexOf("<a");
-			if (index == -1) {
-				builder.append(originalText);
-				break;
-			}
-			builder.append(originalText.substring(0, index));
-			originalText = originalText.substring(index);
-
-			index = originalText.indexOf(">");
-			if (index == -1) {
-				break;
-			}
-			String hyperlinkText = originalText.substring(0, index + 1);
-			final int start = hyperlinkText.indexOf("\"");
-			final int end = hyperlinkText.lastIndexOf("\"");
-			if (start == end) {
-				break;
-			}
-			hyperlinkText = hyperlinkText.substring(start + 1, end);
-			hyperlinkText = ZLNetworkUtil.appendParameter(hyperlinkText, null, null);
-
-			originalText = originalText.substring(index + 1);
-
-			index = originalText.indexOf("</a>");
-			if (index == -1) {
-				break;
-			}
-			final int len = builder.length();
-			builder.append(originalText.substring(0, index));
-			builder.setSpan(new URLSpan(hyperlinkText), len, len + index, 0);
-			originalText = originalText.substring(index + 4);
-		}
-		
-		return builder.subSequence(0, builder.length());
-	}
-
 }
